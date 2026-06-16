99e4cfd3defbdaef7b547564aeb5a701bd71703d
Lethicia Silva <lethiciasr2020@gmail.com>
2026-06-16 11:27:03 -0300

    Implementa suporte a subprogramas e correcoes de escopo

---
+++ saida.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+int T1;
+int T2;
+int T3;
+int T4;
+int T5;
+int T6;
+int T7;
+int T8;
+
+
+int calcular_dobro(int T1) {
+resultado = valor * 2;
+return resultado;
+}
+
+int main()
+{
+numero = 7;
+dobro = calcular_dobro(numero);
+printf("%d\n", dobro);
+    return 0;
+}
